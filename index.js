5a66016927420dd994ee2078483f8b0f6634c8f8
Maria T. Andersen <mteilmann93@gmail.com>
2023-07-17 18:10:23 +0200

    Use fetch to grap data sent from the backend. For each property inside of the data array, create a grid element and append it to #gridContainer.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+const gridContainer = document.getElementById("grid-container");
+
+function fetchDataFromBackend() {
+  fetch("http://localhost:5502/", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("all requested data from the backend: ", data);
+
+      data.forEach((element, index) => {
+        console.log(element);
+
+        for (key in element) {
+          createGridElement("div", `${key}: ${element[key]}`, index);
+        }
+      });
+    })
+    .catch((err) => console.error(err));
+}
+
+fetchDataFromBackend();
+
+function createGridElement(HTMLtag, innerText, id) {
+  const newElement = document.createElement(HTMLtag);
+  newElement.innerText = innerText;
+  newElement.classList.add("grid-item");
+  newElement.setAttribute("id", id);
+  gridContainer.appendChild(newElement);
+}
